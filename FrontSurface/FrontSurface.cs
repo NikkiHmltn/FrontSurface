fcaed5778b493c3d2905942237c0249002fc98a1
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-03-01 10:21:52 -0800

    calculated simple front surface area

---
+++ FrontSurface/FrontSurface.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace FrontSurface
+{
+    public class FrontSurface
+    {
+        private int height { get; set; }
+        private int length { get; set; }
+
+        public FrontSurface(int length, int height)
+        {
+            this.length = length;
+            this.height = height;
+           
+        }
+
+        public int Surface()
+        {
+            this.height = height;
+            this.length = length;
+            int SurfaceTotal = height * length;
+            return SurfaceTotal;
+        }
+    }
+}
--- FrontSurface/Program.cs
+++ FrontSurface/Program.cs
@@ -6,7 +6,11 @@ namespace FrontSurface
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //challenge: create a read only property "FrontSurface" which calculates
+            //the front surface based on height and length
+
+            FrontSurface table = new FrontSurface(10, 100);
+            Console.WriteLine("The surface is " + table.Surface());
         }
     }
 }
